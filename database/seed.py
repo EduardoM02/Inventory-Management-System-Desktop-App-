d8d37011b9207cdd3f34acbfc630cdc262d458e0
Eduardo Montaño Gonzalez <eduardomg0212@gmail.com>
2025-12-30 15:24:37 -0700

    Remove debug prints from seed.py
    Removed debugging code from seed.py that printed movement data.

--- database/seed.py
+++ database/seed.py
@@ -1,13 +1,1 @@
-from database import SessionLocal
-from .data import movements_data
-from .repositories import MovementRepository
 
-movement_repo = MovementRepository()
-
-for data in movements_data:
-    print("DEBUG movement data:", data)
-    t = data.get("type")
-    print("  -> type repr:", repr(t))
-    print("  -> type class:", type(t))
-    print("  -> has .value?:", getattr(t, "value", None))
-    movement_repo.add(data)
